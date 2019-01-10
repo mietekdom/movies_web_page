a25c11b74e462e47b57d2f70e4f2cd5c4f974975
mietekdom <mieczyslaw.domansky@gmail.com>
2019-01-10 22:32:53 +0100

    CRUD Items: Create, Read, Update, Delete

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import wszystkie_filmy
+from .views import wszystkie_filmy, nowy_film, edytuj_film, usun_film
 
 urlpatterns = [
-    path('filmy/', wszystkie_filmy),
+    path('filmy/', wszystkie_filmy, name='wszystkie_filmy'),
+    path('nowy/', nowy_film, name='nowy_film'),
+    path('edytuj/<int:id>', edytuj_film, name='edytuj_film'),
+    path('usun/<int:id>', usun_film, name='usun_film'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Movie
+from .forms import MovieForm
 
 # Create your views here.
 
 def wszystkie_filmy(request):
-    text_views = "to jest tekst z views"
-    filmy = ['Terminator', 'Alien', 'Terminator Salvation']
-    return render(request, 'lista_filmow.html',
-                  {'text': text_views, 'filmy': filmy})
+    filmy = Movie.objects.all()
+    return render(request, 'lista_filmow.html', {'filmy': filmy})
 
+def nowy_film(request):
+    form = MovieForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect(wszystkie_filmy)
+    return render(request, 'film_form.html', {'form': form})
+
+def edytuj_film (request, id):
+    film = get_object_or_404(Movie, pk=id)
+    form = MovieForm(request.POST or None, request.FILES or None, instance=film)
+
+    if form.is_valid():
+        form.save()
+        return redirect(wszystkie_filmy)
+    return render(request, 'film_form.html', {'form': form})
+
+def usun_film(request, id):
+    film = get_object_or_404(Movie, pk=id)
+
+    if request.method == 'POST':
+        film.delete()
+        return redirect(wszystkie_filmy)
+    return render(request, 'potwierdz.html', {'film': film})
